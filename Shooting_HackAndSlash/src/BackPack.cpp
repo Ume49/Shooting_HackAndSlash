2eb7bc58fbb44075ff4a1a873cb6c9480fab59e0
K2Cr2O7 <tomoki101014@hoge.com>
2022-01-11 13:41:41 +0900

    入れ替えがBackPackに適用されないバグを修正しました。

--- Shooting_HackAndSlash/src/BackPack.cpp
+++ Shooting_HackAndSlash/src/BackPack.cpp
@@ -117,7 +117,7 @@ namespace Shooting_HackAndSlash::Gun_Custamize {
 		// マウスが重なっているスロットがあるか探索
 		// ついでにそれと合うようにイテレータも移動
 		for (auto& w : slots) {
-			if (w.is_on_mouse() && (*iter == eBullet::Null)) {	// マウスと重なっているスロットが空欄の時のみ許可
+			if (w.is_on_mouse()) {	// マウスと重なっているスロットが空欄の時のみ許可
 
 				if (*iter != eBullet::Null) {	// スロットに中身がある場合の処理
 					// 中身を退避
